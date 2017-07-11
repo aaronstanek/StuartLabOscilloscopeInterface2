73b4731d57a42625d0536c9d4e4f00db05e42627
Aaron Stanek <Aaron Stanek>
2017-07-11 13:14:58 -0700

    added merge, addSpace, and addReturn functions

--- save_text.py
+++ save_text.py
@@ -4,3 +4,16 @@ def dumpBinToFile(filename,data): #data is a list of ASCII values
     outfile.truncate(0)
     outfile.seek(0,0)
     outfile.close()
+
+def merge(data,s):
+    #data is a list of ASCII values
+    #s is a string
+    for x in s:
+        data.append(ord(x))
+
+def addSpace(data):
+    data.append(32)
+
+def addReturn(data):
+    data.append(13)
+    data.append(10)
