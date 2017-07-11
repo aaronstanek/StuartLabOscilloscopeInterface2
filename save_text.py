1f2b4ef0f7a5b7ec3657445afe89c02cd08e9570
Aaron Stanek <Aaron Stanek>
2017-07-11 16:35:05 -0700

    created format text_stuart1

--- save_text.py
+++ save_text.py
@@ -44,3 +44,69 @@ def save_basic(ds,passData):
             passData["fileCount"] = passData["fileCount"]+1
         except:
             print("Error while writing to file. Event skipped.")
+
+def save_stuart1(ds,passData):
+    chan = []
+    for i in range(1,5): #1,2,3,4
+        if i in ds.data[0].data:
+            chan.append(i)
+    #chan now holds the list of channels in order (only those that are present)
+    dataLen = len(ds.data[0].data[chan[0]]) #ugly line
+    for x in ds.data:
+        #for each event
+        try:
+            ouData = []
+            #header
+            merge(ouData,"RIGOL1074Z")
+            addSpace(ouData)
+            merge(ouData,str(x.meta["TIME"]))
+            addSpace(ouData)
+            merge(ouData,str(dataLen))
+            addSpace(ouData)
+            merge(ouData,str(len(chan)))
+            addSpace(ouData)
+            merge(ouData,str(ds.meta["DISPLAY_TIMEDIVISION"]))
+            addSpace(ouData)
+            merge(ouData,str(ds.meta["DISPLAY_VOLTAGEDIVISION"]))
+            addReturn(ouData)
+            merge(ouData,str(ds.meta["TRIGGER_MODE"]))
+            addSpace(ouData)
+            merge(ouData,str(ds.meta["TRIGGER_POINT"]))
+            addReturn(ouData)
+            if ds.meta["TRIGGER_MODE"]=="EDGE":
+                merge(ouData,str(ds.meta["TRIGGER_CHANNEL"]))
+                addSpace(ouData)
+                merge(ouData,str(ds.meta["TRIGGER_SLOPE"]))
+                addSpace(ouData)
+                merge(ouData,str(ds.meta["TRIGGER_LEVEL"]))
+                addReturn(ouData)
+            elif ds.meta["TRIGGER_MODE"]=="DEL":
+                merge(ouData,str(ds.meta["TRIGGER_SOURCEA"]))
+                addSpace(ouData)
+                merge(ouData,str(ds.meta["TRIGGER_SLOPEA"]))
+                addSpace(ouData)
+                merge(ouData,str(ds.meta["TRIGGER_SOURCEB"]))
+                addSpace(ouData)
+                merge(ouData,str(ds.meta["TRIGGER_SLOPEB"]))
+                addSpace(ouData)
+                merge(ouData,str(ds.meta["TRIGGER_DELAYTYPE"]))
+                addSpace(ouData)
+                merge(ouData,str(ds.meta["TRIGGER_MAXDELAY"]))
+                addSpace(ouData)
+                merge(ouData,str(ds.meta["TRIGGER_MINDELAY"]))
+                addReturn(ouData)
+            else:
+                merge(ouData,"-----")
+                addReturn(ouData)
+            #data
+            for line in range(dataLen):
+                for i in range(len(chan)):
+                    if i!=0:
+                        addSpace(ouData)
+                    merge(ouData,str(x.data[chan[i]][line]))
+                addReturn(ouData)
+            dumpBinToFile(passData["path"]+"event_"+str(passData["eventCount"])+".ord.txt",ouData)
+            passData["eventCount"] = passData["eventCount"]+1
+            passData["fileCount"] = passData["fileCount"]+1
+        except:
+            print("Error while writing to file. Event skipped.")
