ef849518ff80f85e7a0ecdbc92f00d2ddd876744
Aaron Stanek <nope>
2017-08-02 23:06:16 -0700

    added common functions to save_ords.py

---
+++ save_ords.py
@@ -0,0 +1,20 @@
+def dumpBinToFile(filename,data): #data is a list of ASCII values
+    bdata = bytes(data)
+    outfile = open(filename,"wb")
+    outfile.truncate(0)
+    outfile.seek(0,0)
+    outfile.write(bdata)
+    outfile.close()
+
+def merge(data,s):
+    #data is a list of ASCII values
+    #s is a string
+    for x in s:
+        data.append(ord(x))
+
+def addSpace(data):
+    data.append(32)
+
+def addReturn(data):
+    data.append(13)
+    data.append(10)
